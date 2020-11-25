fix wrap overflow, issue #20

fxpmath/objects.py
@@ -451,7 +451,7 @@ class Fxp():
             #     val = np.array(max(val_min, min(val_max, new_val)))
             val = utils.clip(new_val, val_min, val_max)
         elif self.overflow == 'wrap':
-            val = utils.wrap(new_val, val_min, val_max, self.signed, self.n_word)
+            val = utils.wrap(new_val, self.signed, self.n_word)
         return val
 
     def _round(self, val, method='floor'):

tests/test_performace.py
@@ -31,7 +31,7 @@ def test_perf_wrap(signed=True, n_word=8, repeat=10):
     exec_time_vals = np.zeros(repeat)
     for i in range(repeat):
         start_time = time.time()
-        utils.wrap(np.random.uniform(low=-512, high=512, size=[1000,2]), val_min=-64, val_max=64, signed=signed, n_word=n_word )
+        utils.wrap(np.random.uniform(low=-512, high=512, size=[1000,2]), signed=signed, n_word=n_word)
         exec_time_vals[i] = time.time() - start_time
     print('\nutils.wrap execution time over {} repetitions'.format(repeat))
     print('\tmean = {:.3f} ms\n\tstd = {:.3f} ms'.format(np.mean(exec_time_vals)*1e3, np.std(exec_time_vals)*1e3))