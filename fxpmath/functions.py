fix issue #31 * Fix casting to object in extended precision multiply. * Fix casting to object inside constructor.

fxpmath/functions.py
@@ -335,7 +335,8 @@ def mul(x, y, out=None, out_like=None, sizing='optimal', method='raw', **kwargs)
     """
     def _mul_raw(x, y, n_frac):
         precision_cast = (lambda m: np.array(m, dtype=object)) if n_frac >= _n_word_max else (lambda m: m)
-        return x.val * y.val * precision_cast(2**(n_frac - x.n_frac - y.n_frac))
+        raw_cast = (lambda m: np.array(m, dtype=object)) if (x.n_word + y.n_word) >= _n_word_max else (lambda m: m)
+        return raw_cast(x.val) * raw_cast(y.val) * precision_cast(2**(n_frac - x.n_frac - y.n_frac))
 
     if not isinstance(x, Fxp):
         x = Fxp(x)

fxpmath/objects.py
@@ -596,6 +596,7 @@ class Fxp():
             _n_word_max_ = min(_n_word_max, 64)
             if np.max(val) >= 2**_n_word_max_ or np.min(val) < -2**_n_word_max_ or self.n_word >= _n_word_max_:
                 val_dtype = object
+                val = val.astype(object)
             else:
                 val_dtype = np.int64 if self.signed else np.uint64
 
@@ -608,7 +609,7 @@ class Fxp():
 
             if val_dtype == object:       
                 # convert each element to int
-                new_val = np.array(list(map(int, new_val.flatten()))).reshape(new_val.shape)
+                new_val = np.array(list(map(int, new_val.flatten()))).reshape(new_val.shape).astype(val_dtype)
             
             if index is not None:
                 self.val[index] = new_val